7dc66b9803e8e6e314d031f63b9686b0101c44a1
Pedro Olympio Ayres Begnini <78813946+pedroayresb@users.noreply.github.com>
2022-11-20 00:57:28 -0300

    conserta docker

--- server/build/util/database.js
+++ server/build/util/database.js
@@ -2,8 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 var sequelize_1 = require("sequelize");
-var db = new sequelize_1.Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
-    host: process.env.HOST,
+var db = new sequelize_1.Sequelize('ng-project', 'postgres', 'postgres_password', {
+    host: 'postgres',
+    port: 5432,
     dialect: 'postgres',
 });
 exports.db = db;
